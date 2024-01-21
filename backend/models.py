7a2bc0baa93ded1a38ae72d5fd8c926d8583377d
abhijeetlokhande1996 <abhijeetlokhande1996@gmail.com>
2024-01-21 08:43:36 +0000

    Product table added

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20)
+    price = models.FloatField()
+    description = models.TextField()
+    primary_colour = models.CharField(max_length=20)
