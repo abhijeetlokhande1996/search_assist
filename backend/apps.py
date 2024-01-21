de5b18ecea50d1450cb429039d01af045cf7c191
abhijeetlokhande1996 <abhijeetlokhande1996@gmail.com>
2024-01-21 22:02:31 +0000

    in ready signal faiss index loaded

--- backend/apps.py
+++ backend/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from config import DATA_DIR
+from config import faiss_index
+import faiss
 
 
 class BackendConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "backend"
+
+    def ready(self):
+        index_filepath = str(DATA_DIR / "products.index")
+        index = faiss.read_index(index_filepath)
+        faiss_index = index
+        print("Faiss Index loaded and assigned")
